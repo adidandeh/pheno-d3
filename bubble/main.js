e1f0dd2082dc9c33089a875b849b6247ebd2714c
Jonathan Demelo <demelode@gmail.com>
2014-11-13 04:55:59 -0500

    tool tips added demoing content

--- bubble/main.js
+++ bubble/main.js
@@ -18,11 +18,16 @@ var svg = d3.select("body").append("svg")
     .attr("height", diameter)
     .attr("class", "bubble");
 
+var div = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
+
 d3.selection.prototype.size = function() {
   var n = 0;
   this.each(function() { ++n; });
   return n;
 };
+
 
 defaultFilter = function(d) { 
   return (!d.children)
@@ -56,7 +61,26 @@ function generate(filter, search) {
 
     node.append("circle")
         .attr("r", function(d) { return d.r; })
-        .style("fill", function(d) { return color(d.id); });
+        .style("fill", function(d) { return color(d.id); }).on("mouseover", function(d) {      
+            div.transition()        
+                .duration(200)      
+                .style("opacity", 10);      
+            div .html(
+              "<h3>" + d.medline_article_title + "</h3><br/>"+
+              "<b>Journal Title:</b> " + d.medline_journal_title[0] + "<br/>"+
+              "<b>Publishing Year:</b> " + d.medline_pub_year + "<br/>" +
+              "<b>Author(s):</b> " + d.medline_author_name + "<br/>"+
+              "<b>Phenotype(s):</b> " + d.phenotypes + "<br/>"+
+              "<b>Descriptor(s):</b> " + d.medline_descriptor_name + "<br/>"
+              )  
+                .style("left", (d3.event.pageX - 340) + "px")     
+                .style("top", (d3.event.pageY + 128) + "px");    
+            })                  
+        .on("mouseout", function(d) {       
+            div.transition()        
+                .duration(1000)      
+                .style("opacity", 0);   
+        });
 
     node.append("text")
           .text(function(d) { return d.medline_journal_title; })
