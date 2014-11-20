e15aeb826478c8e3c6f8ffa77e618e7d3d3fbfd9
Jonathan Demelo <demelode@gmail.com>
2014-11-20 08:50:36 -0500

    Trying to get it to update/reorder pheno bar upon click

--- dropbox/main.js
+++ dropbox/main.js
@@ -30,6 +30,28 @@ var data = [{"name": "Integument", "order": 1, "active": 0},
             {"name": "Skeletal System", "order": 21, "active": 0}
           ]
 
+// helper func
+getNumOfActivePheno = function() {
+  var count = 0;
+
+    for (var i=0, l=data.length; i<l; i++) {
+      if(data[i].active == 1) {
+        count++;
+      }
+    }
+
+  return count;
+};
+
+//helper func
+findWithAttr = function (array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+}
+
 var svg = d3.select("#phenobar").append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -43,11 +65,11 @@ var bar = svg.selectAll("g")
              .enter().append("g")
              .attr("transform",
                 function(d,i) {
-                  return "translate(" + i + ", "+ sqwidth +")";
+                  return "translate(" + i + ", "+ sqheight +")";
                 });
 
 bar.append("rect")
-  .attr("x", function(d) { return d.order * (sqwidth + sqspacing); })
+  .attr("x", function(d,i ) { return d.order * (sqwidth); })
   .attr("y", phenobarheight)
   .attr("width", sqwidth)
   .attr("height", sqheight)
@@ -57,15 +79,55 @@ bar.append("rect")
   .on("click", function(d) {
     var rec = d3.select(this); // clicked rec
 
+    // pull current pheno out of the data array.
+    var removedArr = data.splice(findWithAttr(data, 'name', d.name), 1);
+
     // toggle color between two choices
     if (rec.style("fill") == "rgb(255, 0, 0)") {
       rec.style("fill", "green"); 
       rec.attr("class","active");
-      d.active = 1;
+
+      // include itself
+      var numOfActivePheno = getNumOfActivePheno() + 1; 
+
+      // reorder pheno data list
+      // make sure that the new pheno isn't already in the proper place.
+      if(removedArr[0].order != numOfActivePheno){
+        // if it isn't, bump the order of all the right obj elems
+        for (var i=numOfActivePheno-1, l=data.length; i<l; i++) {
+          data[i].order++;
+        }
+      }
+      // adjust the pheno object for insertion
+      removedArr[0].active = 1;
+      removedArr[0].order = numOfActivePheno;
+      // push the changed pheno into the data list at new place.
+      data.splice(numOfActivePheno, 0, removedArr[0]);
+
     } else {
       rec.style("fill", "red");
       rec.attr("class","inactive");
-      d.active = 0;
+
+      // get number of phenos still active
+      var numOfActivePheno = getNumOfActivePheno(); 
+
+      // reorder pheno data list
+      // make sure that the new pheno isn't already in the proper place.
+      // i.e. it's the rightmost of the active slots, and thus can stay
+      // as it is.
+      if(removedArr[0].order != numOfActivePheno){
+        // if it isn't, bump the still active phenos down starting
+        // from it's origin position up to the right most active
+        // pheno.
+        for (var i=removedArr[0].order-1, l=numOfActivePheno; i<l; i++) {
+          data[i].order--;
+        }
+      }
+      // put it's position at the leftmost inactive.
+      removedArr[0].active = 0;
+      removedArr[0].order = numOfActivePheno + 1;
+      // push the changed pheno into the data list at new place.
+      data.splice(numOfActivePheno, 0, removedArr[0]);
     }
   })
   .on("mouseover", function(d) {      
@@ -108,139 +170,3 @@ bar.append("text")
   .style("text-anchor", "middle")
   .attr("pointer-events", "none")
   .text(function(d) { return d.name.substring(0,5); });
-
-
-
-// var i = 0,
-//     duration = 750,
-//     root;
-
-// var tree = d3.layout.tree()
-//     .size([height, width]);
-
-// var diagonal = d3.svg.diagonal()
-//     .projection(function(d) { return [d.y, d.x]; });
-
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width + margin.right + margin.left)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// d3.json("data.json", function(error, flare) {
-//   root = flare;
-//   root.x0 = height / 2;
-//   root.y0 = 0;
-
-//   function collapse(d) {
-//     if (d.children) {
-//       d._children = d.children;
-//       d._children.forEach(collapse);
-//       d.children = null;
-//     }
-//   }
-
-//   root.children.forEach(collapse);
-//   update(root);
-// });
-
-// d3.select(self.frameElement).style("height", "800px");
-
-// function update(source) {
-
-//   // Compute the new tree layout.
-//   var nodes = tree.nodes(root).reverse(),
-//       links = tree.links(nodes);
-
-//   // Normalize for fixed-depth.
-//   nodes.forEach(function(d) { d.y = d.depth * 200; }); // How wide it gets
-
-//   // Update the nodes…
-//   var node = svg.selectAll("g.node")
-//       .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-//   // Enter any new nodes at the parent's previous position.
-//   var nodeEnter = node.enter().append("g")
-//       .attr("class", "node")
-//       .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-//       .on("click", click);
-
-//   nodeEnter.append("circle")
-//       .attr("r", 1e-6)
-//       .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-//   nodeEnter.append("text")
-//       .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-//       .attr("dy", ".35em")
-//       .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-//       .text(function(d) { return d.name; })
-//       .style("fill-opacity", 1e-6);
-
-//   // Transition nodes to their new position.
-//   var nodeUpdate = node.transition()
-//       .duration(duration)
-//       .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-//   nodeUpdate.select("circle")
-//       .attr("r", 4.5)
-//       .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-//   nodeUpdate.select("text")
-//       .style("fill-opacity", 1);
-
-//   // Transition exiting nodes to the parent's new position.
-//   var nodeExit = node.exit().transition()
-//       .duration(duration)
-//       .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-//       .remove();
-
-//   nodeExit.select("circle")
-//       .attr("r", 1e-6);
-
-//   nodeExit.select("text")
-//       .style("fill-opacity", 1e-6);
-
-//   // Update the links…
-//   var link = svg.selectAll("path.link")
-//       .data(links, function(d) { return d.target.id; });
-
-//   // Enter any new links at the parent's previous position.
-//   link.enter().insert("path", "g")
-//       .attr("class", "link")
-//       .attr("d", function(d) {
-//         var o = {x: source.x0, y: source.y0};
-//         return diagonal({source: o, target: o});
-//       });
-
-//   // Transition links to their new position.
-//   link.transition()
-//       .duration(duration)
-//       .attr("d", diagonal);
-
-//   // Transition exiting nodes to the parent's new position.
-//   link.exit().transition()
-//       .duration(duration)
-//       .attr("d", function(d) {
-//         var o = {x: source.x, y: source.y};
-//         return diagonal({source: o, target: o});
-//       })
-//       .remove();
-
-//   // Stash the old positions for transition.
-//   nodes.forEach(function(d) {
-//     d.x0 = d.x;
-//     d.y0 = d.y;
-//   });
-// }
-
-// // Toggle children on click.
-// function click(d) {
-//   if (d.children) {
-//     d._children = d.children;
-//     d.children = null;
-//   } else {
-//     d.children = d._children;
-//     d._children = null;
-//   }
-//   update(d);
-// }
