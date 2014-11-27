127444726e6b9c966442358db71f760593369553
Jonathan Demelo <demelode@gmail.com>
2014-11-27 17:24:21 -0500

    More code cleanup, moved the hardcoded pheno bar data out of the main js file.

---
+++ dropbox/data.js
@@ -0,0 +1,86 @@
+// hardcoded pheno bar data
+var data = [{
+    "name": "Integument",
+    "order": 1,
+    "active": 0
+}, {
+    "name": "Genitourinary System",
+    "order": 2,
+    "active": 0
+}, {
+    "name": "Head and Neck",
+    "order": 3,
+    "active": 0
+}, {
+    "name": "Endocrine",
+    "order": 4,
+    "active": 0
+}, {
+    "name": "Connective Tissue",
+    "order": 5,
+    "active": 0
+}, {
+    "name": "Immune System",
+    "order": 6,
+    "active": 0
+}, {
+    "name": "Abdomen",
+    "order": 7,
+    "active": 0
+}, {
+    "name": "Voice",
+    "order": 8,
+    "active": 0
+}, {
+    "name": "Musculature",
+    "order": 9,
+    "active": 0
+}, {
+    "name": "Cardiovascular System",
+    "order": 10,
+    "active": 0
+}, {
+    "name": "Eye",
+    "order": 11,
+    "active": 0
+}, {
+    "name": "Metabolism/Homeostasis",
+    "order": 12,
+    "active": 0
+}, {
+    "name": "Ear",
+    "order": 13,
+    "active": 0
+}, {
+    "name": "Neoplasm",
+    "order": 14,
+    "active": 0
+}, {
+    "name": "Growth",
+    "order": 15,
+    "active": 0
+}, {
+    "name": "Prenatal or Birth",
+    "order": 16,
+    "active": 0
+}, {
+    "name": "Blood and Blood-Forming Tissue",
+    "order": 17,
+    "active": 0
+}, {
+    "name": "Nervous System",
+    "order": 18,
+    "active": 0
+}, {
+    "name": "Breast",
+    "order": 19,
+    "active": 0
+}, {
+    "name": "Respiratory System",
+    "order": 20,
+    "active": 0
+}, {
+    "name": "Skeletal System",
+    "order": 21,
+    "active": 0
+}]
