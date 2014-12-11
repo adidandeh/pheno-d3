ad84d387e510a10d30161b06dd707f6be10369a9
Jonathan Demelo <demelode@gmail.com>
2014-12-11 15:40:43 -0500

    Adjust hardcoded data.js

--- dropbox/data.js
+++ dropbox/data.js
@@ -2,85 +2,106 @@
 var data = [{
     "name": "Integument",
     "order": 1,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Genitourinary System",
     "order": 2,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Head and Neck",
     "order": 3,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Endocrine",
     "order": 4,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Connective Tissue",
     "order": 5,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Immune System",
     "order": 6,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Abdomen",
     "order": 7,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Voice",
     "order": 8,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Musculature",
     "order": 9,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Cardiovascular System",
     "order": 10,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Eye",
     "order": 11,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Metabolism/Homeostasis",
     "order": 12,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Ear",
     "order": 13,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Neoplasm",
     "order": 14,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Growth",
     "order": 15,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Prenatal or Birth",
     "order": 16,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Blood and Blood-Forming Tissue",
     "order": 17,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Nervous System",
     "order": 18,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Breast",
     "order": 19,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Respiratory System",
     "order": 20,
-    "active": 0
+    "active": 0,
+    "children": []
 }, {
     "name": "Skeletal System",
     "order": 21,
-    "active": 0
+    "active": 0,
+    "children": []
 }]
