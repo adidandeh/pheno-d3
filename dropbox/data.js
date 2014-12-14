b5caa6582088197a268404e14a6010789ce9997c
Jonathan Demelo <demelode@gmail.com>
2014-12-14 15:53:04 -0500

    data.js cleanup, prepping for root pheno name adjustment back to original version.

--- dropbox/data.js
+++ dropbox/data.js
@@ -3,127 +3,105 @@ var data = [{
     "name": "Integument",
     "order": 1,
     "active": 0,
-    "ArrOrder": 0,
-
     "children": []
 }, {
     "name": "Genitourinary System",
     "order": 2,
     "active": 0,
-    "ArrOrder": 1,
     "children": []
 }, {
     "name": "Head and Neck",
     "order": 3,
     "active": 0,
-    "ArrOrder": 2,
     "children": []
 }, {
     "name": "Endocrine",
     "order": 4,
     "active": 0,
-    "ArrOrder": 3,
     "children": []
 }, {
     "name": "Connective Tissue",
     "order": 5,
     "active": 0,
-    "ArrOrder": 4,
     "children": []
 }, {
     "name": "Immune System",
     "order": 6,
     "active": 0,
-    "ArrOrder": 5,
     "children": []
 }, {
     "name": "Abdomen",
     "order": 7,
     "active": 0,
-    "ArrOrder": 6,
     "children": []
 }, {
     "name": "Voice",
     "order": 8,
     "active": 0,
-    "ArrOrder": 7,
     "children": []
 }, {
     "name": "Musculature",
     "order": 9,
     "active": 0,
-    "ArrOrder": 8,
     "children": []
 }, {
     "name": "Cardiovascular System",
     "order": 10,
     "active": 0,
-    "ArrOrder": 9,
     "children": []
 }, {
     "name": "Eye",
     "order": 11,
     "active": 0,
-    "ArrOrder": 10,
     "children": []
 }, {
     "name": "Metabolism/Homeostasis",
     "order": 12,
     "active": 0,
-    "ArrOrder": 11,
     "children": []
 }, {
     "name": "Ear",
     "order": 13,
     "active": 0,
-    "ArrOrder": 12,
     "children": []
 }, {
     "name": "Neoplasm",
     "order": 14,
     "active": 0,
-    "ArrOrder": 13,
     "children": []
 }, {
     "name": "Growth",
     "order": 15,
     "active": 0,
-    "ArrOrder": 14,
     "children": []
 }, {
     "name": "Prenatal or Birth",
     "order": 16,
     "active": 0,
-    "ArrOrder": 15,
     "children": []
 }, {
     "name": "Blood and Blood-Forming Tissue",
     "order": 17,
     "active": 0,
-    "ArrOrder": 16,
     "children": []
 }, {
     "name": "Nervous System",
     "order": 18,
     "active": 0,
-    "ArrOrder": 17,
     "children": []
 }, {
     "name": "Breast",
     "order": 19,
     "active": 0,
-    "ArrOrder": 18,
     "children": []
 }, {
     "name": "Respiratory System",
     "order": 20,
     "active": 0,
-    "ArrOrder": 19,
     "children": []
 }, {
     "name": "Skeletal System",
     "order": 21,
     "active": 0,
-    "ArrOrder": 20,
     "children": []
 }]
